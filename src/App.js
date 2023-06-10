3bb694b700ffd8d53db7a7c4a2e0caf841851759
John Jacob Go <johnjacobgo@gmail.com>
2023-06-11 00:12:52 +0800

    changed colorscheme

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="w-full h-auto bg-bodyColor text-lightText px-4">
       <Navbar />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,10 @@ module.exports = {
         titleFont: ["Montserrat", "sans-serif"],
       },
       colors: {
-        bodyColor: "#212428",
-        lightText: "#c4cfde",
-        boxBg: "linear-gradient(145deg, #1e2024, #23272b)",
-        designColor: "#ff014f",
+        bodyColor: "#fbfcf8",
+        lightText: "#000000",
+        boxBg: "linear-gradient(145deg, #f0f2eb, #fcfff5)",
+        designColor: "#43D8C9",
       },
       boxShadow: {
         shadowOne: "10px 10px 19px #1c1e22, -10px -10px 19px #262a2e",
