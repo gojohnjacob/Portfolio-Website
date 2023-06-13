d90560ef0d2d645ba89a59cc1de18f687963e39d
John Jacob Go <johnjacobgo@gmail.com>
2023-06-13 19:34:35 +0800

    populated skills

--- src/components/resume/Skills.jsx
+++ src/components/resume/Skills.jsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import SkillCard from './SkillCard';
+import { skillsLeftData, skillsRightData } from '../../constants/skillsData';
 
 const Skills = () => (
     <motion.div
@@ -10,33 +11,28 @@ const Skills = () => (
         <div className="w-full lgl:w-1/2">
             <div className="py-12 font-titleFont flex flex-col gap-4">
                 <p className="text-sm text-designColor tracking-[4px] uppercase">
-                    Features
+                    My Tech Stack
                 </p>
-                <h2 className="text-3xl md:text-4xl font-bold">Stack</h2>
+                <h2 className="text-3xl md:text-4xl font-bold">Languages and Frameworks</h2>
             </div>
             <div className="w-full h-auto py-5 flex flex-col gap-6 outline outline-1 bg-transparent bg-opacity-25 text-black text-xl inline-flex justify-center rounded-md ">
-                <SkillCard skillName="JavaScript" rating={4} />
-                <SkillCard skillName="JavaScript" rating={4} />
+                {skillsLeftData.map((item) => (
+                    <SkillCard skillName={item.name} rating={item.rating} />
+                ))}
             </div>
         </div>
 
         <div className="w-full lgl:w-1/2">
             <div className="py-12 font-titleFont flex flex-col gap-4">
                 <p className="text-sm text-designColor tracking-[4px] uppercase">
-                    Features
+                    Other
                 </p>
-                <h2 className="text-3xl md:text-4xl font-bold">Other Skill</h2>
+                <h2 className="text-3xl md:text-4xl font-bold">Tools and Utilities</h2>
             </div>
             <div className="w-full h-auto py-5 flex flex-col gap-6 outline outline-1 bg-transparent bg-opacity-25 text-black text-xl inline-flex justify-center rounded-md ">
-                <SkillCard skillName="JavaScript" rating={4} />
-                <SkillCard skillName="JavaScript" rating={4} />
-                <SkillCard skillName="JavaScript" rating={4} />
-                <SkillCard skillName="JavaScript" rating={4} />
-                <SkillCard skillName="JavaScript" rating={4} />
-                <SkillCard skillName="JavaScript" rating={4} />
-                <SkillCard skillName="JavaScript" rating={4} />
-                <SkillCard skillName="JavaScript" rating={4} />
-                
+                {skillsRightData.map((item) => (
+                    <SkillCard skillName={item.name} rating={item.rating} />
+                ))}
             </div>
         </div>
     </motion.div>
---
+++ src/constants/skillsData.js
@@ -0,0 +1,156 @@
+export const skillsLeftData = [
+    {
+        id: 1,
+        name: "React.JS",
+        rating: 5,
+    },
+    {
+        id: 2,
+        name: "Vue.JS",
+        rating: 4,
+    },
+    {
+        id: 3,
+        name: "Express.JS",
+        rating: 4,
+    },
+    {
+        id: 4,
+        name: "Django",
+        rating: 5,
+    },
+    {
+        id: 5,
+        name: "Flask",
+        rating: 5,
+    },
+    {
+        id: 6,
+        name: "Tailwind CSS",
+        rating: 5,
+    },
+    {
+        id: 7,
+        name: "Bootstrap",
+        rating: 5,
+    },
+    {
+        id: 8,
+        name: "Python",
+        rating: 5,
+    },
+    {
+        id: 9,
+        name: "C",
+        rating: 4,
+    },
+    {
+        id: 10,
+        name: "JavaScript",
+        rating: 5,
+    },
+    {
+        id: 11,
+        name: "TypeScript",
+        rating: 3,
+    },
+    {
+        id: 12,
+        name: "Golang",
+        rating: 3,
+    },
+    {
+        id: 13,
+        name: "Java",
+        rating: 2,
+    },
+    {
+        id: 14,
+        name: "R",
+        rating: 5,
+    },
+    {
+        id: 15,
+        name: "OCaml",
+        rating: 3,
+    },
+];
+
+export const skillsRightData = [
+    {
+        id: 1,
+        name: "REST API",
+        rating: 5,
+    },
+    {
+        id: 2,
+        name: "MongoDB",
+        rating: 4,
+    },
+    {
+        id: 3,
+        name: "PostgreSQL",
+        rating: 4,
+    },
+    {
+        id: 4,
+        name: "MySQL",
+        rating: 5,
+    },
+    {
+        id: 5,
+        name: "Docker",
+        rating: 4,
+    },
+    {
+        id: 6,
+        name: "Git",
+        rating: 5,
+    },
+    {
+        id: 7,
+        name: "GraphQL",
+        rating: 2,
+    },
+    {
+        id: 8,
+        name: "AWS",
+        rating: 3,
+    },
+
+    {
+        id: 9,
+        name: "Kubernetes",
+        rating: 2,
+    },
+    {
+        id: 10,
+        name: "Jenkins",
+        rating: 3,
+    },
+    {
+        id: 11,
+        name: "Linux",
+        rating: 3,
+    },
+    {
+        id: 12,
+        name: "Pocketbase",
+        rating: 4,
+    },
+    {
+        id: 13,
+        name: "Firebase",
+        rating: 2,
+    },
+    {
+        id: 14,
+        name: "Nginx",
+        rating: 3,
+    },
+    {
+        id: 15,
+        name: "Tableau",
+        rating: 3,
+    }
+];
