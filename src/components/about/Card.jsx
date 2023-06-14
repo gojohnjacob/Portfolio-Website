0ba4ad59cafa9a88152d7929c07d51be2a8b13e0
John Jacob Go <johnjacobgo@gmail.com>
2023-06-14 10:53:52 +0800

    removed hover in about

--- src/components/about/Card.jsx
+++ src/components/about/Card.jsx
@@ -2,10 +2,10 @@
 
 const Card = ({item:{title,desc,icon}}) => {
   return (
-    // bg-gradient-to-r from-bodyColor to-[#c4f2ee] hover:bg-gradient-to-b hover:from-[#45c4b9] hover:to-[designColor] transition-colors duration-100
+    // group-hover:translate-y-0 transition-transform duration-500 bg-gradient-to-r from-bodyColor to-[#c4f2ee] hover:bg-gradient-to-b hover:from-[#45c4b9] hover:to-[designColor] transition-colors duration-100
     <div className="w-full px-12 h-80 py-10 rounded-lg flex items-center outline outline-1 group">
         <div className="h-72 overflow-y-hidden">
-        <div className="flex h-full flex-col gap-10 translate-y-5 group-hover:translate-y-0 transition-transform duration-500">
+        <div className="flex h-full flex-col gap-10 translate-y-5">
             <div className="w-10 h-8 flex flex-col justify-between">
                 <span className="text-5xl text-designColor">{icon}</span>
             </div>
